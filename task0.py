87a1415a2670a6cae53ef6fb3a810676b2a13224
DrachArtem <85665996+DrachArtem@users.noreply.github.com>
2021-06-20 23:15:35 +0300

    Update task0.py

--- task0.py
+++ task0.py
@@ -1,46 +1,39 @@
-від  випадкового  імпорту  randint
-
-
-def  rand_arr ():
-    RArr  = []
-    для  i  в  діапазоні ( 0 , 30 ):
-        RArr . append ( randint ( - 100 , 100 ))
-    повернути  RArr
-
-
-def  max_in_arr ( arr ):
-
-    arr_i  =  список ( перерахувати ( arr , 0 ))
-    max_i  =  max ( arr_i , ключ = lambda  i : i [ 1 ])
-
-    повернути  max_i [ 1 ], max_i [ 0 ]
-
-
-def  unpaired_arr ( arr ):
-    un_p_arr  = []
-    для  i  in  arr :
-        якщо  i  %  2  ! =  0 :
-            un_p_arr . додати ( i )
-    якщо  len ( un_p_arr ) ==  0 :
-        повернути  "немає непарних номерів"
-    ще :
-        повернути  un_p_arr
-
-
-arr  =  rand_arr ()
-print ( "початковий випадковий список: \ n " , обр. )
-print ( "max num>" , max_in_arr ( arr ) [ 0 ], " \ n положення максимального числа>" , max_in_arr ( arr ) [ 1 ] + 1 )
-
-# isinstance () перевіряє належність об'єкта до певного типу даних isinstance (об'єкт, тип даних)
-якщо  isinstance ( unpaired_arr ( обр ), список ):
-    print ( "Список неспарених чисел, відсортованих від найвищого до найнижчого: \ n " ,
-          відсортовано ( неспарений_арр ( обр ), зворотний = True ))
-ще :
-    print ( unpaired_arr ( arr ))
-
-
-# 1-Сформувати список із 30 випадкових цілих чисел від -100 до + 100.
-# Знайдіть максимальний список продуктів та його порядковий номер.
-# Отримати інший список, що створюється лише з непарних чисел вихідного списку
-# або повідомити, що таких чисел немає.
-# Отриманий список виводити за зменшенням елементів.
+from random import randint 
+ 
+ 
+def rand_arr(): 
+    RArr = [] 
+    for i in range(0, 30): 
+        RArr.append(randint(-100, 100)) 
+    return RArr 
+ 
+ 
+def max_in_arr(arr): 
+ 
+    arr_i = list(enumerate(arr, 0)) 
+    max_i = max(arr_i, key=lambda i: i[1]) 
+ 
+    return max_i[1], max_i[0] 
+ 
+ 
+def unpaired_arr(arr): 
+    un_p_arr = [] 
+    for i in arr: 
+        if i % 2 != 0: 
+            un_p_arr.append(i) 
+    if len(un_p_arr) == 0: 
+        return "no unpaired numbers" 
+    else: 
+        return un_p_arr 
+ 
+ 
+arr = rand_arr() 
+print("initial random list :\n", arr) 
+print("max num > ", max_in_arr(arr)[0], "\nposition of max num > ", max_in_arr(arr)[1]+1) 
+ 
+# isinstance() проверяет принадлежность объекта к определенному типу данных isinstance(объект,тип данных) 
+if isinstance(unpaired_arr(arr), list): 
+    print("List of unpaired numbers sorted from highest to lowest :\n", 
+          sorted(unpaired_arr(arr), reverse=True)) 
+else: 
+    print(unpaired_arr(arr))
